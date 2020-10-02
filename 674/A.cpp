6ad0f2910b2132ca8674b2b02be811f0b7969132
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2020-10-02 23:27:40 +0530

    Update A.cpp

--- 674/A.cpp
+++ 674/A.cpp
@@ -8,9 +8,7 @@ int main()
     {
         int n{};
         int x = {};
- 
         std::cin >> n >> x ;
- 
         int floor{};
  
         if ((n == 1) || (n == 2))
@@ -21,7 +19,6 @@ int main()
         {
             floor = 2 + (n - 3) / x ;
         }
- 
         std::cout << floor << "\n";
     }
     return 0;
